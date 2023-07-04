22849cc4755c6dff7f23eb6147ca34131f22f819
Gabriel Getzie <ggetzie@hku.hk>
2023-07-04 14:45:05 +0800

    s3 manifest

--- puzzlee/utils/storages.py
+++ puzzlee/utils/storages.py
@@ -1,7 +1,7 @@
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage, S3ManifestStaticStorage
 
 
-class StaticRootS3Boto3Storage(S3Boto3Storage):
+class StaticRootS3Boto3Storage(S3ManifestStaticStorage):
     location = "static"
     # default_acl = "public-read"
 
