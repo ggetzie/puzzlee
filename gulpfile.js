17442d49c84ef3a552ee796dd35bb0e35a4743bc
Gabriel Getzie <ggetzie@hku.hk>
2023-07-06 11:17:55 +0800

    browsers-list

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,28 @@
 ////////////////////////////////
 
 // Gulp and package
-const { src, dest, parallel, series, watch } = require('gulp')
-const pjson = require('./package.json')
+const { src, dest, parallel, series, watch } = require("gulp");
+const pjson = require("./package.json");
 
 // Plugins
-const autoprefixer = require('autoprefixer')
-const browserSync = require('browser-sync').create()
-const concat = require('gulp-concat')
-const cssnano = require ('cssnano')
-const imagemin = require('gulp-imagemin')
-const pixrem = require('pixrem')
-const plumber = require('gulp-plumber')
-const postcss = require('gulp-postcss')
-const reload = browserSync.reload
-const rename = require('gulp-rename')
-const sass = require('gulp-sass')(require('sass'))
-const spawn = require('child_process').spawn
-const uglify = require('gulp-uglify-es').default
+const autoprefixer = require("autoprefixer");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const cssnano = require("cssnano");
+const imagemin = require("gulp-imagemin");
+const pixrem = require("pixrem");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const reload = browserSync.reload;
+const rename = require("gulp-rename");
+const sass = require("gulp-sass")(require("sass"));
+const spawn = require("child_process").spawn;
+const uglify = require("gulp-uglify-es").default;
 
 // Relative paths function
 function pathsConfig(appName) {
-  this.app = `./${pjson.name}`
-  const vendorsRoot = 'node_modules'
+  this.app = `./${pjson.name}`;
+  const vendorsRoot = "node_modules";
 
   return {
     bootstrapSass: `${vendorsRoot}/bootstrap/scss`,
@@ -39,10 +39,10 @@ function pathsConfig(appName) {
     fonts: `${this.app}/static/fonts`,
     images: `${this.app}/static/images`,
     js: `${this.app}/static/js`,
-  }
+  };
 }
 
-const paths = pathsConfig()
+const paths = pathsConfig();
 
 ////////////////////////////////
 // Tasks
@@ -51,27 +51,26 @@ const paths = pathsConfig()
 // Styles autoprefixing and minification
 function styles() {
   const processCss = [
-      autoprefixer(), // adds vendor prefixes
-      pixrem(),       // add fallbacks for rem units
-  ]
+    autoprefixer(), // adds vendor prefixes
+    pixrem(), // add fallbacks for rem units
+  ];
 
   const minifyCss = [
-      cssnano({ preset: 'default' })   // minify result
-  ]
+    cssnano({ preset: "default" }), // minify result
+  ];
 
   return src(`${paths.sass}/project.scss`)
-    .pipe(sass({
-      includePaths: [
-        paths.bootstrapSass,
-        paths.sass
-      ]
-    }).on('error', sass.logError))
+    .pipe(
+      sass({
+        includePaths: [paths.bootstrapSass, paths.sass],
+      }).on("error", sass.logError)
+    )
     .pipe(plumber()) // Checks for errors
     .pipe(postcss(processCss))
     .pipe(dest(paths.css))
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(rename({ suffix: ".min" }))
     .pipe(postcss(minifyCss)) // Minifies the result
-    .pipe(dest(paths.css))
+    .pipe(dest(paths.css));
 }
 
 // Javascript minification
@@ -79,80 +78,71 @@ function scripts() {
   return src(`${paths.js}/project.js`)
     .pipe(plumber()) // Checks for errors
     .pipe(uglify()) // Minifies the js
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(paths.js))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest(paths.js));
 }
 
 // Vendor Javascript minification
 function vendorScripts() {
   return src(paths.vendorsJs)
-    .pipe(concat('vendors.js'))
+    .pipe(concat("vendors.js"))
     .pipe(dest(paths.js))
     .pipe(plumber()) // Checks for errors
     .pipe(uglify()) // Minifies the js
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(paths.js))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest(paths.js));
 }
 
 // Image compression
 function imgCompression() {
   return src(`${paths.images}/*`)
     .pipe(imagemin()) // Compresses PNG, JPEG, GIF and SVG images
-    .pipe(dest(paths.images))
+    .pipe(dest(paths.images));
 }
 // Run django server
 function runServer(cb) {
-  const cmd = spawn('python', ['manage.py', 'runserver'], {stdio: 'inherit'})
-  cmd.on('close', function(code) {
-    console.log('runServer exited with code ' + code)
-    cb(code)
-  })
+  const cmd = spawn("python", ["manage.py", "runserver"], { stdio: "inherit" });
+  cmd.on("close", function (code) {
+    console.log("runServer exited with code " + code);
+    cb(code);
+  });
 }
 
 // Browser sync server for live reload
 function initBrowserSync() {
   browserSync.init(
-    [
-      `${paths.css}/*.css`,
-      `${paths.js}/*.js`,
-      `${paths.templates}/*.html`
-    ], {
+    [`${paths.css}/*.css`, `${paths.js}/*.js`, `${paths.templates}/*.html`],
+    {
       // https://www.browsersync.io/docs/options/#option-proxy
-      proxy:  {
-        target: '127.0.0.1:8000',
+      proxy: {
+        target: "127.0.0.1:8000",
         proxyReq: [
-          function(proxyReq, req) {
+          function (proxyReq, req) {
             // Assign proxy "host" header same as current request at Browsersync server
-            proxyReq.setHeader('Host', req.headers.host)
-          }
-        ]
-      }
+            proxyReq.setHeader("Host", req.headers.host);
+          },
+        ],
+      },
     }
-  )
+  );
 }
 
 // Watch
 function watchPaths() {
-  watch(`${paths.sass}/*.scss`, styles)
-  watch(`${paths.templates}/**/*.html`).on("change", reload)
-  watch([`${paths.js}/*.js`, `!${paths.js}/*.min.js`], scripts).on("change", reload)
+  watch(`${paths.sass}/*.scss`, styles);
+  watch(`${paths.templates}/**/*.html`).on("change", reload);
+  watch([`${paths.js}/*.js`, `!${paths.js}/*.min.js`], scripts).on(
+    "change",
+    reload
+  );
 }
 
 // Generate all assets
-const generateAssets = parallel(
-  styles,
-  scripts,
-  vendorScripts,
-  imgCompression
-)
+const generateAssets = parallel(styles, scripts, vendorScripts, imgCompression);
 
 // Set up dev environment
-const dev = parallel(
-  runServer,
-  initBrowserSync,
-  watchPaths
-)
+const dev = parallel(runServer, initBrowserSync, watchPaths);
 
-exports.default = series(generateAssets, dev)
-exports["generate-assets"] = generateAssets
-exports["dev"] = dev
+exports.default = series(generateAssets, dev);
+exports["generate-assets"] = generateAssets;
+exports["dev"] = dev;
